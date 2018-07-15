3bcd3702cf5fdc6ebb66cd28f2093f4ea54f4461
kylelingat <kylejlingat@gmail.com>
2018-07-14 15:30:56 -1000

    hover animations

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var gameModes = [document.getElementById('modern'),
+                 document.getElementById('allTime'),
+                 document.getElementById('warriors'),
+                 document.getElementById('allDefense'),
+                 document.getElementById('allLakers'),
+                 document.getElementById('allCeltics')
+];
+
+
+
+function selectMode(mode){
+}
